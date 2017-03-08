9e68631644ab282e0ec7bfb83f8da977d2e86e91
superstarlks <supperstarlk@gmail.com>
2017-03-08 15:02:19 +0700

    comit lesson8

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -3,8 +3,14 @@ import {Component} from '@angular/core';
 @Component({
     selector:'my-tutorial',
     template:`<h2>This Angular2 Tutorial component aaaaaa</h2>
-    `
+                <h3 [class.redColor] = "applyClass"> Apply Class </h3>
+                <h4 [style.color] = "blueColor?'blue':'orange'" >Apply style</h4>
+    `,
+    styles:[`.redColor{
+        color:red;
+    }`]
 })
 export class TutorialComponent{
-
+    public applyClass = true; //áp dụng class, =false ko áp dung
+    public blueColor = false; //áp dụng style, =false ko áp dung
 }
