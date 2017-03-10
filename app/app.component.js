a69641121e20d7832873d33bd36ca6950dd71e35
Le Trung Hung <supperstarlk@gmail.com>
2017-03-10 13:37:07 +0700

    lesson16

--- app/app.component.js
+++ app/app.component.js
@@ -19,7 +19,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        templateUrl: 'app/app.component.html'
+        templateUrl: 'app/app.component.html',
+        styleUrls: ['app/app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
